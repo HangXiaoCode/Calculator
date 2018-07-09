21360982caf577b06dd551f67b635a92b84b4640
James Xiao <xiaohang@outlook.com>
2018-07-09 17:15:47 -0400

    now simplify the functions by passing the arguments of each number to number buttons

--- cal.py
+++ cal.py
@@ -18,25 +18,8 @@ count = 0
 
 result = 0
 
-#for i in range(9):
-#    def callback(i):
-#        global display
-#        global t1
-#        global process
-#        global result
-#        global point
-#        global count
-#        if point == 0:
-#            display = display * 10 + i
-#        else:
-#            count = count + 1
-#            display = display + i / (10 ** count)
-#        T.delete('1.0', END)
-#        T.insert(END, display)
 
-
-
-def callback1():
+def callback(i):
     global display
     global t1
     global process
@@ -44,147 +27,14 @@ def callback1():
     global point
     global count
     if point ==0:
-        display = display * 10 + 1
+        display = display * 10 + i
     else:
         count = count +1
-        display = display + 1/(10**count)
+        display = display + i/(10**count)
     T.delete('1.0', END)
     T.insert(END, display)
 
-def callback2():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 2
-    else:
-        count = count +1
-        display = display + 2/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
 
-def callback3():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 3
-    else:
-        count = count +1
-        display = display + 3/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback4():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 4
-    else:
-        count = count +1
-        display = display + 4/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback5():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 5
-    else:
-        count = count +1
-        display = display + 5/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback6():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 6
-    else:
-        count = count +1
-        display = display + 6/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback7():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 7
-    else:
-        count = count +1
-        display = display + 7/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback8():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 8
-    else:
-        count = count +1
-        display = display + 8/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback9():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 9
-    else:
-        count = count +1
-        display = display + 9/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
-
-def callback0():
-    global display
-    global t1
-    global process
-    global result
-    global point
-    global count
-    if point ==0:
-        display = display * 10 + 0
-    else:
-        count = count +1
-        display = display + 0/(10**count)
-    T.delete('1.0', END)
-    T.insert(END, display)
 
 
 
@@ -327,34 +177,34 @@ def callbackpoint():
 
 
 
-MyButton1 = Button(master, text="1", width=10, command=callback1)
+MyButton1 = Button(master, text="1", width=10, command=lambda:callback(1))
 MyButton1.grid(row=0, column=0)
 
-MyButton2 = Button(master, text="4", width=10, command=callback4)
+MyButton2 = Button(master, text="4", width=10, command=lambda:callback(4))
 MyButton2.grid(row=1, column=0)
 
-MyButton3 = Button(master, text="7", width=10, command=callback7)
+MyButton3 = Button(master, text="7", width=10, command=lambda:callback(7))
 MyButton3.grid(row=2, column=0)
 
-MyButton4 = Button(master, text="2", width=10, command=callback2)
+MyButton4 = Button(master, text="2", width=10, command=lambda:callback(2))
 MyButton4.grid(row=0, column=1)
 
-MyButton5 = Button(master, text="5", width=10, command=callback5)
+MyButton5 = Button(master, text="5", width=10, command=lambda:callback(5))
 MyButton5.grid(row=1, column=1)
 
-MyButton6 = Button(master, text="8", width=10, command=callback8)
+MyButton6 = Button(master, text="8", width=10, command=lambda:callback(8))
 MyButton6.grid(row=2, column=1)
 
-MyButton7 = Button(master, text="3", width=10, command=callback3)
+MyButton7 = Button(master, text="3", width=10, command=lambda:callback(3))
 MyButton7.grid(row=0, column=2)
 
-MyButton8 = Button(master, text="6", width=10, command=callback6)
+MyButton8 = Button(master, text="6", width=10, command=lambda:callback(6))
 MyButton8.grid(row=1, column=2)
 
-MyButton9 = Button(master, text="9", width=10, command=callback9)
+MyButton9 = Button(master, text="9", width=10, command=lambda:callback(9))
 MyButton9.grid(row=2, column=2)
 
-MyButton0 = Button(master, text="0", width=10, command=callback0)
+MyButton0 = Button(master, text="0", width=10, command=lambda:callback(0))
 MyButton0.grid(row=3, column=1)
 
 MyButtonplus = Button(master, text="+", width=10, command=callbackplus)
